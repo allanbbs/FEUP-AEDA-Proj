ca8c5329a24ef61c0eebf48a1b24f3d919447405
Allan <allanbp.br@gmail.com>
2019-10-11 18:06:40 +0100

    CLion startup

---
+++ cmake-build-debug/CMakeFiles/AEDA_Proj1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AEDA_Proj1"
+  "AEDA_Proj1.pdb"
+  "CMakeFiles/AEDA_Proj1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AEDA_Proj1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/AEDA_Proj1.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for AEDA_Proj1.
+# This may be replaced when dependencies are built.
