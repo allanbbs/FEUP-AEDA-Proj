f330d52d9721cae8f01ce31d80c3d8220afc2a34
jumaruba <juliane.marbayashi@gmail.com>
2019-10-14 14:53:06 +0100

    Local e servicos

---
+++ Header/Local.h
@@ -0,0 +1,14 @@
+//
+// Created by maruba on 14/10/19.
+//
+
+#ifndef AEDA_PROJ1_LOCAL_H
+#define AEDA_PROJ1_LOCAL_H
+
+
+class Local {
+
+};
+
+
+#endif //AEDA_PROJ1_LOCAL_H
---
+++ Source/Local.cpp
@@ -0,0 +1,5 @@
+//
+// Created by maruba on 14/10/19.
+//
+
+#include "Header/Local.h"
