78cbf645c735a295a43adcb6efc4bd3f55f0a0b9
Allan <allanbp.br@gmail.com>
2020-01-03 00:40:00 +0000

    comments done again

--- Header/Workshop.h
+++ Header/Workshop.h
@@ -10,6 +10,11 @@
 #include <algorithm>
 #include <iostream>
 #include <iomanip>
+
+/**
+ * @file Motorista.h
+ * @brief It contains the Class Motorista and Workers that manages the BST of workers
+ */
 using namespace std;
 
 
@@ -19,21 +24,73 @@ private:
     vector<string> brands;
     int unavailability;
 public:
+    /**
+     * @brief Default constructor
+     */
     explicit Workshop(string name) : name(name){};
+    /**
+     * Constructor for the Motorista
+     * @param name The name of the workshop
+     * @param aux vector containing the brands in which this workshops specializes in
+     * @param nani Initial unavailability
+     */
     Workshop(string name,vector<string> &aux,int nani);
+    /**
+     * @brief Gets the unavailability of the workshop
+     * @return int
+     */
     int get_unavailability() const {return unavailability;};
+    /**
+     * @brief Gets the Workshop's name
+     * @return string
+     */
     string getName() const{return name;};
+    /**
+     * @brief Gets the vector containing brands
+     * @return vector of strings(brands)
+     */
     vector<string> getBrands()const{return brands;};
+    /**
+     * @brief Checks if this Workshop contains given brand
+     * @param name brand to check for
+     * @return int
+     */
     bool checkForBrand(string name)const;
+    /**
+     * @brief Order workshops in ascending order of unavailability
+     * @param w1 the Workshop to compare with
+     * @return True: if this Workshop 's unavailability is greater than w1's,False otherwise
+     */
     bool operator<(const Workshop &w1)const;
+    /**
+     * @brief Checks for equality between two workshops
+     * @param w1 Workshop to compare with
+     * @return True if the names are equal,False otherwise
+     */
     bool operator==(const Workshop& w1) const { return name==w1.getName();};
+    /**
+     * @brief Display workshop info in a human-friendly way
+     * @param out output stream to write to
+     * @param w workshop to display
+     * @return output stream used
+     */
     friend ostream& operator<<(ostream& out, Workshop w);
+    /**
+     * @brief Sets the unavailability of the workshop
+     * @param a new unavailability
+     */
     void setUn(int a) { unavailability = a;};
+    /**
+     * @brief Adds new brand to the workshop
+     * @param n name of the new brand to be added
+     */
     void addBrand(string n){brands.push_back(n);};
     //Workshop& operator=(const Workshop &w1);
 
 };
-
+/**
+     * @brief Prints table to display workshops
+     */
 void headerWorkshop();
 
 
